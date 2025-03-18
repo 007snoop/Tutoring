53e4d7162d62cfd15ab7adc8e24da00d3d8d261a
Colin Yetman <colinguitarist@gmail.com>
2025-03-18 14:58:08 -0230

    fix commit

---
+++ index.js
@@ -0,0 +1,45 @@
+/* let quote = "colin chris, hello! how are you??";
+let quote2 = 'colin chris, hello! how are you??';
+let Stringliteral = `colin chirs ${regex} colin chris, hello! how are you??`;
+let quote3 = /colin chris, hello! how are you\?/;
+ */
+/* let regex = /\.\w+$/; */ // "." | wsjhfabf | + "end" 
+// .{word} => at the end of the string
+/* 
+
+\. = a (.) 
+\w+ = one or more characters ({a-zA-Z0-9})
+$ = the end of the string
+*/
+const regex = new RegExp("\\.com$")
+console.log(regex.test("skfjbsahfhsdbfbgfjjnaskf com"))
+
+
+/* 
+
+. = matching any character ESCAPE \.
+
+\ = escaping the character itself ESCAPE \\
+
++ = one or more of previous ESCAPE \+
+
+* = zero or more of previous ESCAPE \*
+
+? = zero or one of previous ESCAPE \?
+ 
+^ = start of string ESCAPE \^
+
+` = backtick === | OR operate
+
+{} = Quantifer and quantifer is a list of things that might be there. ESCAPE 
+ "\{
+ apple, orange, banana
+ \}"
+
+[] = character sets that must atleast be there. ESCAPE 
+
+() = Grouping character together ESCAPE 
+
+*/
+
+
